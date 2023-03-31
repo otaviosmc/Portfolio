308809cad903d8223adb244d4672c25a13943df5
Otavio Simoncini <contato.otaviosimoncini@gmail.com>
2023-03-31 19:40:17 -0300

    Corrigido erro links certificados

--- src/components/Conteudo/Formacao/index.js
+++ src/components/Conteudo/Formacao/index.js
@@ -14,17 +14,42 @@ export default function Formacao() {
         <div class="diploma">
           <h3>Curso de HTML</h3>
           <p>DevMedia</p>
-          <a href="">Ver Certificado</a>
+          <a
+            target="_blank"
+            href="https://www.devmedia.com.br/certificado/tecnologia/html/otavio-aparecido-simoncini"
+          >
+            Ver Certificado
+          </a>
         </div>
         <div class="diploma">
           <h3>Curso de CSS</h3>
           <p>DevMedia</p>
-          <a href="">Ver Certificado</a>
+          <a
+            target="_blank"
+            href="https://www.devmedia.com.br/certificado/tecnologia/css/otavio-aparecido-simoncini"
+          >
+            Ver Certificado
+          </a>
+        </div>
+        <div class="diploma">
+          <h3>Curso de JS</h3>
+          <p>DevMedia</p>
+          <a
+            target="_blank"
+            href="https://www.devmedia.com.br/certificado/tecnologia/javascript/otavio-aparecido-simoncini"
+          >
+            Ver Certificado
+          </a>
         </div>
         <div class="diploma">
           <h3>Curso de React</h3>
           <p>DevMedia</p>
-          <a href="">Ver Certificado</a>
+          <a
+            target="_blank"
+            href="https://www.devmedia.com.br/certificado/tecnologia/react/otavio-aparecido-simoncini"
+          >
+            Ver Certificado
+          </a>
         </div>
         <div class="diploma">
           <h3>Tecnólogo em ADS</h3>
